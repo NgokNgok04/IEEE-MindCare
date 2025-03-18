004a0251fb66689191578c1e35c548a130fd0dee
NgokNgok04 <117185466+NgokNgok04@users.noreply.github.com>
2025-03-19 01:04:17 +0700

    feat: tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
         primary: "#030014",
         secondary: "#151312",
         light: {
-          100: "#D6C6FF",
+          100: "#75838F",
           200: "#A8B5DB",
           300: "#9CA4AB",
         },
@@ -18,6 +18,21 @@ module.exports = {
           200: "#0f0d23",
         },
         accent: "#AB8BFF",
+        blue: {
+          100: "#428CC3",
+          200: "#DEE5FB",
+          400: "#57A6E0",
+          600: "#2732A5",
+        },
+        grey: {
+          400: "#75838F",
+        },
+        red: {
+          200: "#FF6961",
+        },
+        orange: {
+          400: "#ED7E1C",
+        },
       },
     },
   },
